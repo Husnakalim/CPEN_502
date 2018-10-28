e4be3e86492bb660b17488d601eaa8f54dda99bf
Vetle Birkeland Huglen <vetle.huglen@gmail.com>
2018-10-28 15:56:49 -0700

    added plotfile

--- plot.py
+++ plot.py
@@ -6,4 +6,18 @@ filename = "errortest.txt"
 with open(filename) as textFile:
         lines = [line.split(",") for line in textFile]
 
-print(lines[2])
+"""
+with open(filename) as textFile:
+        data = [[float(value) for value in line.split(",")] for line in textFile]
+"""
+
+data = np.asarray(lines)
+data = data[:, 0:-1]
+data = data.astype(float)
+
+epoch = np.arange(1, 730, 1)
+
+print(data[1:, 8])
+
+plt.plot(epoch, data[21:750, 8])
+plt.show()
