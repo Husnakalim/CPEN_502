5fec8bb43a0ed7c72fad679d45539da52b25e399
Erik Arne Huso <>
2018-10-31 16:52:08 -0700

    small changes

--- plot.py
+++ plot.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 filename = "errortest.txt"
+net_nr = 8
+range = 70
+start_epoch = 10
 
 with open(filename) as textFile:
         lines = [line.split(",") for line in textFile]
@@ -15,9 +18,32 @@ data = np.asarray(lines)
 data = data[:, 0:-1]
 data = data.astype(float)
 
-epoch = np.arange(1, 730, 1)
+epoch = np.arange(1, range, 1)
 
-print(data[1:, 8])
+"""
+plt.figure(1)
+plt.title("Number of epochs versus error")
+plt.plot(epoch, data[start_epoch : start_epoch + range - 1, net_nr])
+#plt.(label)
+plt.show(1)
 
-plt.plot(epoch, data[21:750, 8])
+
+#plt.figure(2)
+for i in range(10):
+        epoch_l = data[0, i]
+        epoch = np.arange(1, epoch_l, 1)
+        plt.plot(epoch, data[:, i])
+
+plt.show()
+"""
+
+
+i = 1
+while i < 10:
+        epoch_l = data[0, i]
+        epoch_l = epoch_l.astype(int)
+        epoch = np.arange(1, epoch_l, 1)
+        plt.plot(epoch, data[1:epoch_l, i])
+        i += 1
+
 plt.show()
